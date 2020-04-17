bef377814a38cb40ed720734b6068971da614dae
Joshua Killen <45966243+joshua-killen@users.noreply.github.com>
2020-04-17 12:53:47 +0100

    .

--- about/about_assets/about.js
+++
@@ -1,9 +0,0 @@
-function hideImg() {
-    var targetImg = document.getElementsByClassName("headImg")
-    
-    if (targetImg.style.backgroundImage == "") { 
-        targetImg.style.backgroundImage= "url('internet.png')"
-    }else if (targetImg.style.backgroundImage == "url('internet.png')") {
-        targetImg.style.backgroundImage= ""
-    }
-}
